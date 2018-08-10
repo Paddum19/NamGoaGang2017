b454bc99bbf9cc0c490064a4c79adfaef7e919a0
Paddum19 <35656143+Paddum19@users.noreply.github.com>
2018-08-11 00:33:00 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,12 @@
 var name = "";
 var x;
 function haveMore(tag) {
-	alert("here");
+	if (tag ==2) {
+		if (name == "") {
+			name = name + document.getElementById("Name").val();
+		} else {
+			name = name + "_" +document.getElementById("Name").val();
+		}
+		alert(name);
+	}
 }
