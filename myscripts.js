ddd17533e07e2f4706d262d95c13b5f7249d177e
Paddum19 <35656143+Paddum19@users.noreply.github.com>
2018-08-11 20:33:11 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -10,24 +10,44 @@ var perHeadAmount=0;
 var finalDetailList="";
 $(document).ready(function(){
 	$("#showDetails").hide();
-	$('#details').on( "click", function() {
-		var y = document.getElementById("showDetails");
-	    if (y.style.display === "none") {
-	        y.style.display = "block";
-	    } else {
-	        y.style.display = "none";
-	    }
+	$("#finalDetails").hide();
+
+	$('#amountSummary').on( "click", function() {
+		$("#finalDetails").show();	
+		$("#showDetails").hide();
+	});
+
+	 
+	
+	$('#reload1').on( "click", function() {
+		window.location.reload();
 	});
 	
+	$('#reload2').on( "click", function() {
+		window.location.reload();
+	});
+	
+	$('#details').on( "click", function() {
+
+		var y = document.getElementById("showDetails");
+		if (y.style.display === "none") {
+			$("#finalDetails").hide();
+			y.style.display = "block";
+		} else {
+			$("#finalDetails").show();
+			y.style.display = "none";
+		}
+	});
+
 	$('#haveMore').on( "click", function() {
 		$("#formDiv_"+count).hide();
 		addingToMap();
-		
+
 		html="<div id=\"formDiv_"+count+"\"><form id=\"form_"+count+"\"><ul class=\"form-style-1\"><li><label>Name:<span class=\"required\">*</span></label><select name=\"field4\" class=\"field-select\"><option value=\"Akshay\">Akshay</option><option value=\"Latha\">Latha</option><option value=\"Likith\">Likith</option><option value=\"Madhurya\">Madhurya</option><option value=\"Mohan\">Mohan</option><option value=\"Pooja\">Pooja</option><option value=\"Pradeep\">Pradeep</option><option value=\"Sushma\">Sushma</option><option value=\"Venki\">Venki</option></select></li><li><label>Amount:<span class=\"required\">*</span></label> <input type=\"text\" name=\"field1\" class=\"field-divided\" placeholder=\"Amount\" />&nbsp;</li><li><label>Reason:</label> <input type=\"text\" name=\"field2\" class=\"field-divided\" placeholder=\"Why Amount Is Spent...\" />&nbsp;</li></ul></form></div>";
 		$('#outerDiv').append(html);  
 	});
-	
-	
+
+
 	$('#finalSubmit').on( "click", function() {
 		addingToMap();
 		perHeadAmount=Object.keys(nameAmountMap).length;
@@ -42,8 +62,10 @@ $(document).ready(function(){
 			}
 		} 
 		$('#finalDetailList').append(finalAmountHtml); 
+		$("#division1").hide();
+		$("#finalDetails").show();
 	});
-	
+
 	function addingToMap(){
 		x=document.getElementById("form_"+count);
 		if (nameAmountMap[x.elements[0].value] === undefined) {
@@ -55,7 +77,7 @@ $(document).ready(function(){
 		showDetails(x.elements[0].value,x.elements[1].value,x.elements[2].value);
 		count++;
 	}
-	
+
 	function showDetails(name,amount,reason){
 		html2="<h3>"+name+"  :: Rs "+amount+"::"+reason+"</h3>";
 		$('#amountList').append(html2);
