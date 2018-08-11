7d9473102f8e3af7be606ac09c7e07c7c62b77f0
Paddum19 <35656143+Paddum19@users.noreply.github.com>
2018-08-11 13:08:07 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,17 @@
-var name = "";
+var nameAmountMap = new Object(); // or var map = {};
+
 var x;
+var html="";
 function haveMore(tag) {
 	x=document.getElementById("form");
 	if (tag ==2) {
-		
-		if (name == "") {
-			name = name + x.elements[0].value ;
-		} else {
-			name = name + "_" +x.elements[0].value ;
-		}
+		html="<div id=\"formDiv_1\"><form id=\"form_1\"><ul class=\"form-style-1\"><li><label>Name:<span class=\"required\">*</span></label><select name=\"field4\" class=\"field-select\"><option value=\"Akshay\">Akshay</option><option value=\"Latha\">Latha</option><option value=\"Likith\">Likith</option><option value=\"Madhurya\">Madhurya</option><option value=\"Mohan\">Mohan</option><option value=\"Pooja\">Pooja</option><option value=\"Pradeep\">Pradeep</option><option value=\"Sushma\">Sushma</option><option value=\"Venki\">Venki</option></select></li><li><label>Amount:<span class=\"required\">*</span></label> <input type=\"text\" name=\"field1\" class=\"field-divided\" placeholder=\"Amount\" />&nbsp;</li><li><label>Reason:</label> <input type=\"text\" name=\"field2\" class=\"field-divided\" placeholder=\"Why Amount Is Spent...\" />&nbsp;</li></ul></form></div>ac";
+		nameAmountMap["name"] = name + x.elements[0].value ;
 		alert(name);
 	}
 }
+$(document).ready(function(){
+	$('#haveMore').on( "click", function() {
+		alert(1);
+	});
+});
