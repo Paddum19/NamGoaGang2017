9766f690023f33e1500f4b4b49b791b2e3e1a595
Paddum19 <35656143+Paddum19@users.noreply.github.com>
2018-08-15 20:09:53 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -8,25 +8,31 @@ var totalAmount=0;
 var finalAmountHtml="";
 var perHeadAmount=0;
 var finalDetailList="";
+var name="";
+var amount="";
+var reason="";
+var status=0;
 $(document).ready(function(){
+
 	$("#showDetails").hide();
 	$("#finalDetails").hide();
+
 
 	$('#amountSummary').on( "click", function() {
 		$("#finalDetails").show();	
 		$("#showDetails").hide();
 	});
 
-	 
-	
+
+
 	$('#reload1').on( "click", function() {
 		window.location.reload();
 	});
-	
+
 	$('#reload2').on( "click", function() {
 		window.location.reload();
 	});
-	
+
 	$('#details').on( "click", function() {
 
 		var y = document.getElementById("showDetails");
@@ -40,16 +46,36 @@ $(document).ready(function(){
 	});
 
 	$('#haveMore').on( "click", function() {
-		$("#formDiv_"+count).hide();
-		addingToMap();
+		x=document.getElementById("form_"+count);
+		name=x.elements[0].value;
+		amount=x.elements[1].value;
+		reason=x.elements[2].value;
+		status=validate(name,amount);
+		if (status==1) {
+			document.getElementById('warningOne'+count).innerHTML='(please select a name)';
+		} else if (status==2) {
+			document.getElementById('warningTwo'+count).innerHTML='(amount cannot be blank)';
+		}else if(status==3){
+			document.getElementById('warningTwo'+count).innerHTML='(amount should be a number)';
+		}else{
+			$("#formDiv_"+count).hide();
+			addingToMap(name,amount,reason);
 
-		html="<div id=\"formDiv_"+count+"\"><form id=\"form_"+count+"\"><ul class=\"form-style-1\"><li><label>Name:<span class=\"required\">*</span></label><select name=\"field4\" class=\"field-select\"><option value=\"Akshay\">Akshay</option><option value=\"Latha\">Latha</option><option value=\"Likith\">Likith</option><option value=\"Madhurya\">Madhurya</option><option value=\"Mohan\">Mohan</option><option value=\"Pooja\">Pooja</option><option value=\"Pradeep\">Pradeep</option><option value=\"Sushma\">Sushma</option><option value=\"Venki\">Venki</option></select></li><li><label>Amount:<span class=\"required\">*</span></label> <input type=\"text\" name=\"field1\" class=\"field-divided\" placeholder=\"Amount\" />&nbsp;</li><li><label>Reason:</label> <input type=\"text\" name=\"field2\" class=\"field-divided\" placeholder=\"Why Amount Is Spent...\" />&nbsp;</li></ul></form></div>";
-		$('#outerDiv').append(html);  
+			html="<div id=\"formDiv_"+count+"\"><form id=\"form_"+count+"\"><ul class=\"form-style-1\"><li id=\"warningOneDiv\"><label>Name:<span class=\"required\">*</span></label> <select name=\"field4\" class=\"field-select\"><option value=\"\">Select</option><option value=\"Akshay\">Akshay</option><option value=\"Latha\">Latha</option><option value=\"Madhurya\">Madhurya</option><option value=\"Mohan\">Mohan</option><option value=\"Pooja\">Pooja</option><option value=\"Pradeep\">Pradeep</option><option value=\"Sushma\">Sushma</option><option value=\"Venki\">Venki</option></select> <span id=\"warningOne"+count+"\" style=\"color: red\"></span></li><li id=\"warningTwoDiv\"><label>Amount:<span class=\"required\">*</span></label> <input type=\"text\" name=\"field1\" class=\"field-divided\" placeholder=\"Amount\" /> <span id=\"warningTwo"+count+"\" style=\"color: red\"></span></li><li><label>Reason:</label> <input type=\"text\" name=\"field2\" class=\"field-divided\" placeholder=\"Why Amount Is Spent...\" />&nbsp;</li></ul></form></div>";
+			$('#outerDiv').append(html); 
+		}
 	});
 
 
 	$('#finalSubmit').on( "click", function() {
-		addingToMap();
+		x=document.getElementById("form_"+count);
+		name=x.elements[0].value;
+		amount=x.elements[1].value;
+		reason=x.elements[2].value;
+		status=validate(name,amount);
+
+
+		addingToMap(name,amount,reason);
 		perHeadAmount=Object.keys(nameAmountMap).length;
 		perHeadAmount=Math.round(parseFloat(totalAmount/perHeadAmount));
 		for (var i in nameAmountMap) {
@@ -66,20 +92,35 @@ $(document).ready(function(){
 		$("#finalDetails").show();
 	});
 
-	function addingToMap(){
-		x=document.getElementById("form_"+count);
-		if (nameAmountMap[x.elements[0].value] === undefined) {
-			nameAmountMap[x.elements[0].value]=x.elements[1].value;
+	function addingToMap(name,amount,reason){
+		if (nameAmountMap[name] === undefined) {
+			nameAmountMap[name]=amount;
 		}else{
-			nameAmountMap[x.elements[0].value]=Math.round(parseFloat(nameAmountMap[x.elements[0].value], 10)+parseFloat(x.elements[1].value, 10))+"";
+			nameAmountMap[name]=Math.round(parseFloat(nameAmountMap[name], 10)+parseFloat(amount, 10))+"";
 		}
-		totalAmount=totalAmount+Math.round(parseFloat(x.elements[1].value));
-		showDetails(x.elements[0].value,x.elements[1].value,x.elements[2].value);
+		totalAmount=totalAmount+Math.round(parseFloat(amount));
+		showDetails(name,amount,reason);
 		count++;
 	}
-
+	var htmltable="";
 	function showDetails(name,amount,reason){
-		html2="<h3>Name: "+name+"             Amount : Rs "+Math.round(parseFloat(amount))+":       Reason :"+reason+"</h3>";
-		$('#amountList').append(html2);
+		if (reason==""||reason==null) {
+			reason="-";
+		}
+		html2="<tr style=\"background-color: #f2f2f2;}\"><td align=\"center\">"+name+"</td><td align=\"center\">"+Math.round(parseFloat(amount))+"</td><td align=\"center\">"+reason+"</td></tr>"
+		$(html2).appendTo("#amountList tbody");
+	}
+
+	function validate(name,amount){
+		document.getElementById('warningOne'+count).innerHTML="";
+		document.getElementById('warningTwo'+count).innerHTML="";
+		if (name==undefined||name=="") {
+			return 1;
+		}else if(amount==undefined||amount==""||amount==null){
+			return 2;
+		}else if(!(/^\d+$/.test(amount))){
+			return 3;
+		}
+		return 4;
 	}
 });
